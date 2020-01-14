9083802a5813f6b6c9c94d5f5d773ec491b71c4f
carladanenberg2 <carladanenberg@gmail.com>
2020-01-14 14:17:33 +0100

    wijzigingen

--- index.php
+++ index.php
@@ -7,28 +7,34 @@
     <meta name="author" content="">
     <script defer src="assets/fontawesome/js/all.js"></script>
     <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="stylesheet" type="text/css" href="assets/css/slitslider/style.css" />
+    <link rel="stylesheet" type="text/css" href="assets/css/slitslider/custom.css" />
     <link href="https://fonts.googleapis.com/css?family=Quicksand:400,500,600&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Big+Shoulders+Text:400,500,600,700,800&display=swap" rel="stylesheet">
     <script defer src="assets/fontawesome/js/all.js"></script>
+	<script type="text/javascript" src="assets/js/modernizr.custom.79639.js"></script>
+	<noscript>
+		<link rel="stylesheet" type="text/css" href="assets/css/slitslider/styleNoJS.css" />
+	</noscript>
 </head>
 <body>
-     <main>
-         <div id="wrapper">
-             <header>
-                 <div class="top-header">
-                     <div class="row d-flex justify-content-around">
+    <main>
+        <div id="wrapper">
+            <header>
+                <div class="top-header">
+                    <div class="row d-flex justify-content-around">
                         <div class="p-2 top-button">
-                        <i class="fas fa-file-invoice"></i> Offerte aanvragen
+                            <i class="fas fa-file-invoice"></i> Offerte aanvragen
                         </div>
                         <div class="p-2 top-button">
-                        <i class="fas fa-phone"></i> 06-23582650
+                            <i class="fas fa-phone"></i> 06-23582650
                         </div>
                         <div class="p-2 top-button">
-                        <i class="fas fa-envelope"></i> Info@dtwdakramen.nl
+                            <i class="fas fa-envelope"></i> Info@dtwdakramen.nl
                         </div>
-                     </div>
-                 </div>
-                 <div class="navigation-wrapper">
+                    </div>
+                </div>
+                <div class="navigation-wrapper">
                     <div class="container">
                         <!-- start navigation -->
                         <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -66,24 +72,251 @@
                                         </div>
                                     </li>
                                     <li class="nav-item">
-                                    <a class="nav-link" href="contact.php">Prijzen</a>
+                                        <a class="nav-link" href="contact.php">Prijzen</a>
                                     </li>
                                     <li class="nav-item">
-                                    <a class="nav-link" href="contact.php">Contact</a>
+                                        <a class="nav-link" href="contact.php">Contact</a>
                                     </li>
                                 </ul>
-                        
                             </div>
                         </nav>
-                        <!-- END navigation -->
+                            <!-- END navigation -->
                     </div>
-                 </div>
-             </header>
-             <section id="featured" class="slitslider">
-               
-                </section>
-            </div>
-        </main>   
+                </div>
+            </header>
+            <section id="featured" class="slitslider">
+                <!-- slitslider -->
+                <div id="slider" class="sl-slider-wrapper">
+
+                    <div class="sl-slider">
+                        
+                        <div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="-25" data-slice2-rotation="-25" data-slice1-scale="2" data-slice2-scale="2">
+                            <div class="sl-slide-inner">
+                                <div class="bg-img bg-img-1"></div>
+                                    <h2>Kwaliteit</h2>
+                                    <blockquote><p>Een dakraam plaatsen valt en staat met de juiste afwerking</p></blockquote>
+                                </div>
+                            </div>
+                            
+                            <div class="sl-slide" data-orientation="vertical" data-slice1-rotation="10" data-slice2-rotation="-15" data-slice1-scale="1.5" data-slice2-scale="1.5">
+                                <div class="sl-slide-inner">
+                                    <div class="bg-img bg-img-2"></div>
+                                    <h2>Daglicht</h2>
+                                    <blockquote><p>Breng dachtlicht in uw huis</p></blockquote>
+                                </div>
+                            </div>
+                            
+                            <div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="3" data-slice2-rotation="3" data-slice1-scale="2" data-slice2-scale="1">
+                                <div class="sl-slide-inner">
+                                    <div class="bg-img bg-img-3"></div>
+                                    <h2>Montage partner</h2>
+                                    <blockquote><p>Montage partner van VELUX, FAKRO en Baskapel</blockquote>
+                                </div>
+                            </div>
+                            
+                            <div class="sl-slide" data-orientation="vertical" data-slice1-rotation="-5" data-slice2-rotation="25" data-slice1-scale="2" data-slice2-scale="1">
+                                <div class="sl-slide-inner">
+                                    <div class="bg-img bg-img-4"></div>
+                                    <h2></h2>
+                                    <blockquote><p></p></blockquote>
+                                </div>
+                            </div>
+                            
+                            <div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="-5" data-slice2-rotation="10" data-slice1-scale="2" data-slice2-scale="1">
+                                <div class="sl-slide-inner">
+                                    <div class="bg-img bg-img-5"></div>
+                                    <h2></h2>
+                                    <blockquote><p></p></blockquote>
+                                </div>
+                            </div>
+                        </div><!-- /sl-slider -->
+
+                        <nav id="nav-dots" class="nav-dots">
+                            <span class="nav-dot-current"></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </nav>
+
+			        </div><!-- /slider-wrapper -->
+                 <!-- end slitslider -->
+                </div>
+                <!--banner block-->
+                <div class="row d-flex justify-content-center banner-home">
+                    <div class="banner-block">
+                        <div class="p-4">
+                            <div class="banner-block-content">
+                            <span class="icon"><i class="fas fa-check"></i></span> 
+                            <span class="content">Tevreden klanten</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="banner-block">
+                        <div class="p-4">
+                            <div class="banner-block-content">
+                            <span class="icon"><i class="fas fa-angle-double-right"></i> </span>
+                            <span class="content">15 jaar ervaring</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="banner-block">
+                        <div class="p-4">
+                            <div class="">
+                                <div class="banner-block-content" >
+                                    <span class="icon"><i class="fas fa-certificate fas-size"></i></span>
+                                     <span class="content">Gecertificeerd partner</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--end banner block-->
+                <!-- info items -->
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                            <div class="blok blok-wit">
+                                <a href="" title="">
+                                    <img src="images/items/dakraam-tips.png" alt="Tips bij de keuze van een dakraam" class="info-img">
+                                </a>
+                                <div class="blok-text">
+                                    <h2>Tips om uw te helpen bij uw keuze</h2>
+                                    <p> Handige tips bij de aankoop van een dakkapel</p>
+                                </div>
+                                <a href="" class="btn"> Tips en Inspiratie</a>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                            <div class="blok blok-wit">
+                                <a href="" title="">
+                                    <img src="images/items/item1.png" alt="" class="info-img">
+                                </a>
+                                <div class="blok-text">
+                                    <h2>Vrijblijvend advies</h2>
+                                    <p> Wij geven u vrijblijvend advies bij de aanschaf van een dakraam</p>
+                                </div>
+                                <a href="" class="btn"> Vrijblijvend</a>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                            <div class="blok blok-wit">
+                                <a href="" title="">
+                                    <img src="images/items/item1.png" alt="" class="info-img">
+                                </a>
+                                <div class="blok-text">
+                                    <h2>Stappenplan aanschaf dakraam</h2>
+                                    <p> Een handig stappenplan van het gehele traject</p>
+                                </div>
+                                <a href="" class="btn"> Stappenplan</a>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                            <div class="blok blok-wit">
+                                <a href="" title="">
+                                    <img src="images/items/item1.png" alt="" class="info-img">
+                                </a>
+                                <div class="blok-text">
+                                    <h2>Vrijblijvende offerte</h2>
+                                    <p> Neem contact met ons op voor een vrijblijvende offerte</p>
+                                </div>
+                                <a href="" class="btn"> Offerte aanvragen</a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- info 2-->
+
+                    <!-- end info 2 -->
+                    <div class="content">
+                        <div class="row">
+                            <div class="col-xs-12 col-md-6">
+                                <div class="content-l">
+                                    <div class="row">
+                                        <div class="col-xs-12 col-md-6">
+                                        <img src="images/logos/velux.png" alt="velux dakramen">
+                                        </div>
+                                        <div class="col-xs-12 col-md-6 subcontent">
+                                            <p class="bold">DTW dakramen is een erkend montagepartner</p>
+                                        </div>
+                                    </div>
+                                </div>
+                              
+                            </div>
+                            <div class="col-xs-12 col-md-6">
+                            <div class="content-r">
+                                <div class="row">
+                                        <div class="col-xs-12 col-md-6">
+                                            <img src="images/logos/fakro.png"  alt="fakro dakramen">
+                                        </div>
+                                        <div class="col-xs-12 col-md-6 subcontent">
+                                           <p class="bold">DTW dakramen is een erkend montagepartner</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- end info items -->
+                <article class="home-content">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-xs-12 col-md-6">
+                                <h1>DTWdakkapel de dakramen specialist</h1>
+                                <p>
+                                    DTWdakramen heeft al ruim 15 jaar ervaring in het plaatsen van dakramen. Wij begeven ons voornamelijk op de particuliere markt maar ook 
+                                    op de zakelijke markt hebben wij verschillende projecten verzorgd. Wij streven er naar op met kwaliteit en vakmanschap en via een kwalitatieve 
+                                    oplossing meer licht en ruimte in de woning te creeeren. Dit alles gaat gepaard met de juiste prijs/kwaliteit. Wij voorzien uw woning van het 
+                                    juiste dakkapel met een kwalitatief hoge afwerking. Als bedrijf hanteren wij de volgende merken; <strong>VELUX, FAKRO </strong>en <strong>Baskapel</strong>
+                                    wij voorzien u daarom graag van de juiste informatie voor het plaatsen van een dakraam. Neem <a href="">contact</a> met ons op voor het juiste advies!
+                                </p>
+                            </div>
+                            <div class="col-xs-12 col-md-6">
+                                <h2 class="secondary"><strong>Voordelen</strong> van DTWdakramen.nl</h2>
+                                <ul class="check">
+                                    <li>15 jaar ervaring met het plaatsen van dakramen</li>
+                                    <li>Elke woning is ander dus wij plaatsen met maatwerk</li>
+                                    <li>Wij geven ruime garantie op dakramen</li>
+                                    <li>Door gebruik van EPDM-dakbedekking leveren we extra levensduur</li>
+                                    <li>Alleen de beste kwaliteit is voor ons voldoende</li>
+                                    <li>Gecerificeerd in plaatsen van dakramen</li>
+                                    <li>Kwaliteit staat bij ons voorop</li>
+                                </ul>
+
+                            </div>
+                        </div>
+                    </div>
+                </article>
+                
+            </section>
+            <footer class="footer-blok">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12 col-md-4">
+                            <div class="tertaire">
+                                <h3>Contactgegevens</h3>
+                                <strong>DTWdakramen</strong><br>
+                                    KvK:37128304<br/>
+                                    BTW: <br/>
+                                    info@dtwdakramen.nl<br/>
+                                    Tel: 0623582650
+                                
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-md-4">
+                            <a href="#" class="btn button-offerte"> <i class="fas fa-file-invoice"></i> Vraag vrijblijvend een offerte aan  </a>
+                            <a href="#" class="btn button-offerte"> <i class="fas fa-phone"></i> Bel mij terug op een ander tijdstip</a>
+                        </div>
+                        <div class="col-xs-12 col-md-4">
+                            
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </main>   
        
         <!-- Bootstrap core JavaScript -->   
         <script src="assets/js/jquery/jquery-3.3.1.slim.min.js "></script>
@@ -159,5 +392,5 @@
     
         <script src="bootstrap-4/js/bootstrap.min.js "></script>
         <script src="assets/js/dtw-script.js "></script>
-    </body>
+</body>
 </html>
